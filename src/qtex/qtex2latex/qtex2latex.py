df62023cb942b0d3a626e76d6c9e13dc5b912683
Filipe Vasconcelos <filipe.vasconcelos@esme.fr>
2025-11-26 11:46:33 +0100

    coderunner version

--- src/qtex/qtex2latex/qtex2latex.py
+++ src/qtex/qtex2latex/qtex2latex.py
@@ -5,19 +5,25 @@ import re
 import random
 from qtex.common.lib_qtex import quellecle,readqtex,default_values_before 
 
-def benv(name):
-    return "\\begin{"f"{name}""}"
-def eenv(name):
-    return "\\end{"f"{name}""}"
-def macro(name,value):
-    return f"{cmd(name)}{'{'}{value}{'}'}"
-def cmd(name):
-    return f"\\{name}"
+# --------------------------------------------------------------------------
 def newline(size="0.2cm"):
     if len(size) :
         return f"\\\\[{size}]\n"
     else:
         return f"\n"
+# --------------------------------------------------------------------------
+def benv(name):
+    return "\\begin{"f"{name}""}"+newline('')
+# --------------------------------------------------------------------------
+def eenv(name):
+    return "\\end{"f"{name}""}"+newline('')
+# --------------------------------------------------------------------------
+def cmd(name):
+    return f"\\{name}"
+# --------------------------------------------------------------------------
+def macro(name,value):
+    return f"{cmd(name)}{'{'}{value}{'}'}"
+# --------------------------------------------------------------------------
 def lans(answ_text,grad,index,corrige=True):
     out="\\indent\\textbf{"f"{chr(index+65)}.""}"
     if corrige :
@@ -29,14 +35,14 @@ def lans(answ_text,grad,index,corrige=True):
     else:
         return out+f" {answ_text} "+"\\newline\hfill"
     return out
-
+# --------------------------------------------------------------------------
 def multichoice_to_latex(info,outfile,corrige=True):
     outfile.write(macro("question",info["Q"])+newline())
     answ=list(zip(info['ANSW_TEXT'],info['ANSW_GRAD']))
     random.shuffle(answ)
     for k, (answ_txt,answ_grad) in enumerate(answ):
         outfile.write(lans(answ_txt,answ_grad,k,corrige=True)+newline(""))
-
+# --------------------------------------------------------------------------
 def tikz_matching(k,subq,answ_text,gw=80,dw=80):
     #print(k,G,D)
     if k == 0 :
@@ -57,29 +63,37 @@ def tikz_matching(k,subq,answ_text,gw=80,dw=80):
         {cmd('draw')}[thick,outer sep=8mm] (b{k+1}) circle(0.5ex);
         {cmd('draw')}[thick,outer sep=8mm] (c{k+1}) circle(0.5ex);
         {cmd('node')}[below=0.75cm of d{k}.center,anchor=center,text width={dw}mm,align = right] (d{k+1}) {"{"} {macro("textbf",str(k+1)+'.')} {answ_text} {"}"};"""
-
+# --------------------------------------------------------------------------
 def tikz_relie(indices):
     return "\n".join([ f"{cmd('draw')}[very thick] (b{k+1}) -- (c{i+1});" for k,i in enumerate(indices)])
-
+# --------------------------------------------------------------------------
 def matching_to_latex(info,outfile,corrige=True):
-    outfile.write(macro("question",info["Q"])+newline())
-    outfile.write("\\indent\\resizebox{\linewidth}{!}{%"+newline(""))
-    outfile.write(benv("tikzpicture")+newline(""))
+    outfile.write(macro("question",info["Q"]))
+    outfile.write(cmd('indent')+macro('resizebox','\linewidth')+'{!}{%'+newline(""))
+    outfile.write(benv("tikzpicture"))
     maxwidth_sub_q, maxwidth_answ_text = (max(map(len,info['SUB_Q']))+8)*1.51323 , (max(map(len,info['ANSW_TEXT']))+8)*1.51323
     indices=[k for k in range(len(info['SUB_Q']))]
     shuff=list(zip(info['ANSW_TEXT'],indices))
     random.shuffle(shuff)
     shuff_answ_text,indices=zip(*shuff)
     for k,(subq,answ_text) in enumerate(zip(info['SUB_Q'],shuff_answ_text)) :
-        outfile.write(tikz_matching(k,subq,answ_text,gw=maxwidth_sub_q,dw=maxwidth_answ_text)+newline(""))
+        outfile.write(tikz_matching(k,subq,answ_text,gw=maxwidth_sub_q,dw=maxwidth_answ_text))
     if corrige:
-        outfile.write(tikz_relie(indices)+newline(""))
-    outfile.write(eenv("tikzpicture")+newline(""))
-    outfile.write('}'+newline(""))
-
-def coderunner_to_latex(info):
-    pass
-
+        outfile.write(tikz_relie(indices))
+    outfile.write(eenv("tikzpicture"))
+    outfile.write('}')
+# --------------------------------------------------------------------------
+def coderunner_to_latex(info,outfile,numlines=18):
+    outfile.write(macro("question",info["Q"])+newline())
+    outfile.write(benv("tikzpicture"))
+    outfile.write(cmd('draw')+"[ultra thick,inner sep=0] (0,0) rectangle (\linewidth,"+f"{numlines*0.5}cm)"+";\n")
+    outfile.write(cmd('draw')+"[gray!40] (0,0) grid[step=0.5cm](\linewidth,"+f"{numlines*0.5}cm)"+";\n")
+    outfile.write(eenv("tikzpicture"))
+                #\begin{tikzpicture}
+                #    \draw[ultra thick,inner sep=0] (0,0) rectangle (\linewidth,\sizegrid);
+                #    \draw[gray!40] (0,0) grid[step=0.5cm](\linewidth,\sizegrid);
+                #\end{tikzpicture}
+# --------------------------------------------------------------------------
 def qtex_to_latex(info,outfile):
     match info["TYPE"]:
         case "multichoice":
@@ -90,7 +104,6 @@ def qtex_to_latex(info,outfile):
             coderunner_to_latex(info,outfile)
         case _:
             print(f"{info['TYPE']}->latex not possible")
-
 #--------------------------------------------------------------------------------------------------
 # main parsing function
 def parsing():
@@ -111,8 +124,7 @@ def parsing():
     path=os.path.dirname(args.input[0].name)+'/'
     filespath=args.input
     return path,filespath,output
-
-
+#--------------------------------------------------------------------------------------------------
 def html_to_tex(info):
     filters=[]
     filters+=[lambda t : re.sub(r"{", r"\{", t)]
@@ -128,8 +140,7 @@ def html_to_tex(info):
                         info[key]=list(map(f,info[key]))
                 else:
                     info[key]=f(info[key])
-
-
+#--------------------------------------------------------------------------------------------------
 def main():
     path,filespath,outfile=parsing()
     for file in filespath :
@@ -138,6 +149,5 @@ def main():
         info=readqtex(path,file)
         html_to_tex(info)
         print(f"\n\ttype : {info['TYPE']}\n\tname : {info['NAME']}\n\ttags : {info['TAGS']} ",file=sys.stderr)
-#        print(info)
         qtex_to_latex(info,outfile)
 
